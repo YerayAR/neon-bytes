ba02fc761913857bbf338af83edb72a310e1731a
SmartWebApp Developer <smartwebapp@example.com>
2025-07-24 09:49:46 +0100

    \Fix: Corregir patrón glob de MDX en tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './src/app/**/*.{ts,tsx}',
     './src/components/**/*.{ts,tsx}',
-    './src/newsletters/**/*.{mdx}'
+    './src/newsletters/**/*.mdx'
   ],
   theme: {
     extend: {},
